37c7f30e58df333d27230bef47323d395baffc5d
wgcowan <wm.cowan@gmail.com>
2015-06-24 13:44:12 -0400

    Latest Release

--- IuWebsitePlugin.php
+++ IuWebsitePlugin.php
@@ -6,69 +6,25 @@ class IuWebsitePlugin extends Omeka_Plugin_AbstractPlugin
 
     function hookpublichead()
     {
-	?>
-        <!-- BEGIN INDIANA UNIVERSITY BRANDING BAR AND FOOTER <HEAD> ELEMENTS -->
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/css/base.css" rel="stylesheet" type="text/css" />
-        <link href="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/css/wide.css" media="(min-width: 48.000em)" rel="stylesheet" type="text/css" />
+	include "gwassets/brand/1.x/header-iub.html";
+	
+	queue_css_url("/gwassets/brand/1.x/brand.css");
+    }
 
-        <!--[if (lte IE 8) & (!IEMobile)]>
-            <link href="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/css/fixed.css" media="screen" rel="stylesheet" type="text/css" />
-        <![endif]-->
-        <!--[if IE 7]>
-            <link href="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/css/ie7.css" rel="stylesheet" type="text/css" />
-        <![endif]--> 
-        <!--[if IE 6]>
-            <link href="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/css/ie6.css" rel="stylesheet" type="text/css" />
-        <![endif]-->
-        <!-- END INDIANA UNIVERSITY BRANDING BAR AND FOOTER <HEAD> ELEMENTS -->
-    <?php }
-	function hookpublicbody()
-	{?>
-    	<!-- BEGIN INDIANA UNIVERSITY BRANDING BAR -->
-        <div id="branding-bar">
-            <div class="bar">
-                <div class="wrapper">
-                    <p class="campus">
-                        <a href="http://www.iu.edu/">
-                            <img src="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/img/trident-tab.gif" height="73" width="64" alt=" " />
-                            Indiana University
-                        </a>
-                    </p>
-
-                </div>
-            </div>
-        </div>
-        <!-- END INDIANA UNIVERSITY BRANDING BAR -->
-	<?php }
-	function hookpublicfooter()
-	{?>
-        <!-- BEGIN INDIANA UNIVERSITY FOOTER -->
-        <div id="footer" role="contentinfo">
-        	<div class="wrapper">
-				<p class="tagline">
-				Fulfilling
-				<span>the</span>
-				Promise
-				</p>
-                <p class="internal"><a href="http://policies.iu.edu/policies/categories/academic-faculty-students/libraries-archives/libraries-privacy-policy.shtml">Privacy Notice</a></p>
-                <p class="copyright">
-                    <a href="http://www.iu.edu/" class="block-iu"><img src="<?php echo WEB_ROOT;?>/plugins/IuWebsite/views/public/_iu-brand/img/block-iu.png" height="26" width="22" alt="Block IU" /></a> 
-                    <span class="line-break">
-                        <a href="http://www.iu.edu/copyright/index.shtml">Copyright</a> &#169; 
-                        <script type="text/javascript">
-                            var theDate=new Date()
-                            document.write(theDate.getFullYear())
-                        </script>
-                    </span> 
-                    <span class="line-break">The Trustees of <a href="http://www.iu.edu/">Indiana University</a>,</span> 
-                    <span class="line-break"><a href="http://www.iu.edu/copyright/complaints.shtml">Copyright Complaints</a></span>
-                </p>
-            </div>
-        </div>
-        <!-- END INDIANA UNIVERSITY FOOTER -->
-	<?php }
+function hookpublicbody()
+	{
+	}
+	
+function hookpublicfooter()
+	{
+		include "gwassets/brand/1.x/footer.html";
+?>
+		<script>
+		var newPrivacyURL = 'http://www.iu.edu/comments/privacy.shtml';
+		(oldPrivacyURL = document.getElementById('privacy-policy-link')) ? (oldPrivacyURL.href = newPrivacyURL) : '';
+		</script>
+<?php }
+	
 	/*
 	* Admin with jwplayer
 	*/
--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-IU Website Branding
+IU Website
 ===========
 
-Repository for Omeka plugin for Indiana University Branding.
+This plugin adds the official IU Header and Footer Branding to an Omeka site.
 
-Unzip the file and then change the name of the directory to just IuWebsite and upload to your plugins directory. 
+Steps for set up:
 
-If you have a previous version installed, uninstall and delete the directory. Then copy the new IuWebsite to the plugins directory.
+1. in a terminal windows ssh to webserve.iu.edu and login with your Webserve account.
+2. cd (change directory) to your Omeka site such as 'cd www/omeka-2.3'.
+3. execute 'ln -s /ip/gwassets/wwws gwassets'
+4. exit
 
-In addition, each theme in Omeka handles its footer differently so in some cases it works fine and in others you will need to tweak. 
+Download and install the IU Website plugin.
+
+Currently the Privacy Policy links to IU's default policy.
+
+If you want to change where the Privacy Policy links to:
+
+1. Edit IuWebsitePlugin.php in the plugins directory.
+2. find the following block of code:
+	<script>
+	var newPrivacyURL = 'http://www.iu.edu/comments/privacy.shtml';
+	(oldPrivacyURL = document.getElementById('privacy-policy-link')) ? (oldPrivacyURL.href = newPrivacyURL) : '';
+	</script>
+3. Change http://www.iu.edu/comments/privacy.shtml to whichever url contains your department's or school's privacy policy. 
+4. Save file.
+5. Refresh browser page.
+
+Since the page layout can vary with each theme, you may need to modify any theme you use to make sure that the current theme does not contain elements that interfere with the IU Brand Header and/or Footer.
